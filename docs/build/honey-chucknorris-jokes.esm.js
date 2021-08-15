5ddcaaf4fafb9e66bbb8f9a835a70356932e7609
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-08-15 15:00:56 +0200

    [feature/projectsetup] neu gebaut

--- docs/build/honey-chucknorris-jokes.esm.js
+++ docs/build/honey-chucknorris-jokes.esm.js
@@ -1,1 +1,1 @@
-import{p as o,b as r}from"./p-76b0f29c.js";(()=>{const r=import.meta.url,e={};return""!==r&&(e.resourcesUrl=new URL(".",r).href),o(e)})().then((o=>r([["p-617b3df9",[[1,"honey-chucknorris-jokes",{changePeriod:[1026,"period"],witz:[32]}]]]],o)));
+import{p as o,b as r}from"./p-76b0f29c.js";(()=>{const r=import.meta.url,e={};return""!==r&&(e.resourcesUrl=new URL(".",r).href),o(e)})().then((o=>r([["p-905e4529",[[1,"honey-chucknorris-jokes",{changePeriod:[1026,"period"],witz:[32]}]]]],o)));

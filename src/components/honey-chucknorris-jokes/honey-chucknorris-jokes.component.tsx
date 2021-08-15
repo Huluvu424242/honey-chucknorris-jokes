e8583320532908361ee90f8544843add426ea0fe
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-08-15 17:22:13 +0200

    [feature/projectsetup] change period fixed

--- src/components/honey-chucknorris-jokes/honey-chucknorris-jokes.component.tsx
+++ src/components/honey-chucknorris-jokes/honey-chucknorris-jokes.component.tsx
@@ -1,6 +1,6 @@
 import {Component, Element, h, Host, Prop, State, Watch} from "@stencil/core";
 import {Witz} from "./witz";
-import {EMPTY, lastValueFrom, Observable, Subscription, timer} from "rxjs";
+import {EMPTY, from, lastValueFrom, Observable, Subscription, timer} from "rxjs";
 import {fromFetch} from "rxjs/fetch";
 import {catchError, switchMap, tap} from "rxjs/operators";
 
@@ -26,12 +26,6 @@ export class HoneyChucknorrisJokes {
   fetcherSubscription: Subscription;
 
   @State() witz: Witz;
-  // {
-  //   id: "4_kRvuABR7mNQZxh-_UH1A",
-  //   imgurl: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-  //   website: "https://api.chucknorris.io/jokes/4_kRvuABR7mNQZxh-_UH1A",
-  //   text: "Chuck Norris' Ipod came with a real charger instead of just a usb cord."
-  // };
 
   /**
    * Zeitintervall nachdem ein neuer Witz abgerufen wird (in Sekunden).
@@ -39,12 +33,13 @@ export class HoneyChucknorrisJokes {
   @Prop({attribute: "period", mutable: true}) changePeriod: number = 20;
 
   @Watch("changePeriod")
-  newsWatcher(newValue: number, oldValue: number) {
-    console.log("period changed old:" + oldValue + " new:" + newValue);
+  periodWatcher(newValue: number, oldValue: number) {
+    this.printMessage("period changed old:" + oldValue + " new:" + newValue);
     if (newValue && oldValue !== newValue) {
       this.changePeriod = newValue;
-      this.subscribePeriodicFetcher();
-      console.log("period changed to:" + this.changePeriod);
+      this.fetcherSubscription.unsubscribe();
+      this.fetcherSubscription = this.subscribePeriodicFetcher();
+      this.printMessage("period changed to:" + this.changePeriod);
     }
   }
 
@@ -52,23 +47,29 @@ export class HoneyChucknorrisJokes {
     // attribute initialisieren wenn defaults notwendig
     this.ident = this.hostElement.id ? this.hostElement.id : Math.random().toString(36).substring(7);
     this.fetcherSubscription = this.subscribePeriodicFetcher();
-    console.log("DOM connected um: " + (new Date().toUTCString()));
+    this.printMessage("DOM connected um: " + (new Date().toUTCString()));
   }
 
   public async componentWillLoad() {
-    console.log("Lade Daten um:  " + (new Date().toUTCString()));
-    // Für Anschauungszecke von verzögertem Rendering nächste Zeile nutzen oder inet drosseln
-    // await lastValueFrom(timer(5000).pipe(switchMap(() => this.fetchWitz())));
-    await lastValueFrom(this.fetchWitz());
-    console.log("Daten geladen um:  " + (new Date().toUTCString()));
+    this.printMessage("Lade Daten um:  " + (new Date().toUTCString()));
+    // TODO: Fehler beim ersten Fetch -> F5 sonst kein Rendering
+    await lastValueFrom(this.fetchWitz$());
+    this.printMessage("Daten geladen um:  " + (new Date().toUTCString()));
   }
 
   public disconnectedCallback() {
     this.fetcherSubscription.unsubscribe();
-    console.log("DOM disconnected um: " + (new Date().toUTCString()));
+    this.printMessage("DOM disconnected um: " + (new Date().toUTCString()));
+  }
+
+  protected printMessage(message: string): void {
+    if (console) {
+      console.log(message);
+    }
   }
 
   protected setWitz(data: any): void {
+    this.printMessage("setze neuen  Witz um: " + (new Date().toUTCString()))
     if (data) {
       // trigger rendering nur wenn ref changed
       this.witz = {
@@ -77,17 +78,15 @@ export class HoneyChucknorrisJokes {
         website: data.url,
         text: data.value
       };
+    } else {
+      this.printMessage("konnte Witz nicht setzen um: " + (new Date().toUTCString()))
     }
   }
 
-  protected fetchWitz(): Observable<Response> {
+  protected fetchWitz$(): Observable<Response> {
     return fromFetch(HoneyChucknorrisJokes.CHUCK_NORRIS_API_URL).pipe(
-      catchError(() => EMPTY),
       switchMap(
-        (response: Response) => response.json()
-      ),
-      tap(
-        (data: any) => this.setWitz(data)
+        (response: Response) => from(response.json()).pipe(tap((data: any) => this.setWitz(data)), catchError(() => EMPTY))
       )
     )
   }
@@ -96,17 +95,17 @@ export class HoneyChucknorrisJokes {
     const timerPeriod: number = this.changePeriod * 1000;
     const fetcher$: Observable<Response> = timer(timerPeriod, timerPeriod).pipe(
       tap(
-        () => console.log("neuen Witz angefordert um: " + (new Date().toUTCString()))
+        () => this.printMessage("neuen Witz angefordert um: " + (new Date().toUTCString()))
       ),
       switchMap(
-        () => this.fetchWitz()
-      )
+        () => this.fetchWitz$()
+      ),
     );
     return fetcher$.subscribe();
   }
 
   public render() {
-    console.log("rendering um: " + (new Date().toUTCString()));
+    this.printMessage("rendering um: " + (new Date().toUTCString()));
     return (
       <Host
         id={this.ident}

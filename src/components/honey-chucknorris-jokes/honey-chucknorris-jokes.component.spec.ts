88fe7afcd0fd41003eea54af36c987bfa9b0844f
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-08-15 19:49:26 +0200

    [feature/projectsetup] unittest fixed

--- src/components/honey-chucknorris-jokes/honey-chucknorris-jokes.component.spec.ts
+++ src/components/honey-chucknorris-jokes/honey-chucknorris-jokes.component.spec.ts
@@ -1,12 +1,25 @@
 import {newSpecPage} from '@stencil/core/testing';
 import {HoneyChucknorrisJokes} from "./honey-chucknorris-jokes.component";
 
-it('should render my component', async () => {
-  const page = await newSpecPage({
-    components: [HoneyChucknorrisJokes],
-    html: `<honey-chucknorris-jokes id="2"></honey-chucknorris-jokes>`,
-  });
-  expect(page.root).toEqualHtml(`
+declare var fetch: Function;
+
+
+describe('Component Test', () => {
+  // beforeAll(() => {
+  //   fetch = jest.fn(() =>
+  //     Promise.resolve({
+  //       ok: true,
+  //       json: jest.fn(() => Promise.resolve({id: "test", website: "http", imgurl: "imgurl", text: "joke"}))
+  //     })
+  //   );
+  // });
+
+  it('should render my component', async () => {
+    const page = await newSpecPage({
+      components: [HoneyChucknorrisJokes],
+      html: `<honey-chucknorris-jokes id="2"></honey-chucknorris-jokes>`,
+    });
+    expect(page.root).toEqualHtml(`
      <honey-chucknorris-jokes id="2">
      <mock:shadow-root>
        <a class="container" target="blank">
@@ -16,4 +29,12 @@ it('should render my component', async () => {
      </mock:shadow-root>
      </honey-chucknorris-jokes>
   `);
+    page.rootInstance.fetcherSubscription.unsubscribe();
+
+  });
+
+  // afterAll(() => {
+  //   fetch['mockClear']();
+  // });
+
 });

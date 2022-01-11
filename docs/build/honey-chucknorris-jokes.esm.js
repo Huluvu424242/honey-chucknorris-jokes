4a77f7fcbb6aebede0848ae92c5a7476a64461d1
Huluvu424242 <huluvu424242@gmail.com>
2022-01-11 16:39:09 +0100

    rebuild

--- docs/build/honey-chucknorris-jokes.esm.js
+++ docs/build/honey-chucknorris-jokes.esm.js
@@ -1,1 +1,1 @@
-import{p as o,b as r}from"./p-76b0f29c.js";(()=>{const r=import.meta.url,e={};return""!==r&&(e.resourcesUrl=new URL(".",r).href),o(e)})().then((o=>r([["p-9703074f",[[1,"honey-chucknorris-jokes",{changePeriod:[1026,"period"],witz:[32]}]]]],o)));
+import{p as o,b as r}from"./p-17af1838.js";(()=>{const r=import.meta.url,e={};return""!==r&&(e.resourcesUrl=new URL(".",r).href),o(e)})().then((o=>r([["p-199b15cd",[[1,"honey-chucknorris-jokes",{changePeriod:[1026,"period"],witz:[32]}]]]],o)));
